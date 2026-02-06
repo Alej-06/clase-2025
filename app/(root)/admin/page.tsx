92eada67c4be9d6afc6dd557d6ccfc144d387711
Alejandro Pavon <alejandropm06@gmail.com>
2026-02-06 09:36:36 +0100

    Cambios para el next de la clase en la que no estuve

--- app/(root)/admin/page.tsx
+++ app/(root)/admin/page.tsx
@@ -1,30 +1,36 @@
-import React from 'react'
-import { auth } from "@/lib/auth"; // path to your Better Auth server instance
+import React from "react";
+import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
-import SignOutButton from '@/components/auth/sign-out-button';
-import ProductTable from '@/components/admin/product-table';
-import { getProductsTable } from '@/lib/actions/product.actions';
+import SignOutButton from "@/components/auth/sign-out-button";
+import ProductTable from "@/components/admin/product-table";
+import { getProductsTable } from "@/lib/actions/product.actions";
 
-
-
-export default async function AdminPage({searchParams}:{searchParams: Promise<{[key: string]: string | string }>;
+export default async function AdminPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] }>;
 }) {
-    const session = await auth.api.getSession({
-    headers: await headers() // you need to pass the headers object.
-});
-if (!session || session.user.role !== "admin"){
-    return <div>NO AUTORIZADO</div>
-}
-const { page = 1, pageSize = 2 } = await searchParams
-const {data, pageInfo} = await getProductsTable({
-  page:Number(page), 
-  pageSize:Number(pageSize)
-});
-return (
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session || session.user.role !== "admin") {
+    return <div>NO AUTORIZADO</div>;
+  }
+  const { page = 1, pageSize = 2 } = await searchParams;
+  const { data, pageInfo } = await getProductsTable({
+    page: Number(page),
+    pageSize: Number(pageSize),
+  });
+
+  return (
     <>
-    <div>AdminPage</div>
-    <ProductTable products={data} currentPage={pageInfo.currentPage} totalPages={pageInfo.totalPages}/>
-    <SignOutButton />
+      <div>AdminPage</div>
+      <ProductTable
+        products={data}
+        currentPage={pageInfo.currentPage}
+        totalPages={pageInfo.totalPages}
+      />
+      <SignOutButton />
     </>
-  )
+  );
 }
